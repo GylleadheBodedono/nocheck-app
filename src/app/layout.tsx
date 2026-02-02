f13f9caf15a8e221ec69f89a38bae1d18a5e4faf
GylleadheBodedono <ferreira.ti@bodedono.com.br>
2026-02-02 11:58:13 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "API Checklist Fácil",
+  favicon: "/favicon.ico"
   description: "API de conexão com o Checklist Fácil",
 };
 
